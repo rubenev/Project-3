020b5874733b80a00d3e70461387651baa489e8a
Sam_Couwenhoven <Samcouwenhoven@Gmail.com>
2016-04-11 12:48:40 +0200

    Tram class
    + tramclass volledig
    + beschrijvingen
    I stations aangepast

--- ParknTravel/src/Main/IStations.java
+++ ParknTravel/src/Main/IStations.java
@@ -6,7 +6,42 @@ package Main;
  * @author Halfvol
  */
 public interface IStations extends IComponents{
-    public void getStation();
-    public void setStation();
+    /**
+     *
+     * @return a list of the attributes as String
+     */
+    public String[] getStation();
+    /**
+     * gets attributes from the database and sets an instance.
+     * @param Code the unique code in Database
+     * 
+     */
+    public void setStation(String Code);
+    /**
+    * @return the Name
+    **/
+    public String getName();
+    /**
+    * @return the Description
+    **/
+    public String getDescription();
+    /**
+    * @return the Type
+    **/
+    public String getType();
+    /**
+    * @return the Longitude
+    **/
+    public double getLongitude();
+    /**
+    * @return the LAtitude
+    **/
+    public double getLatitude();
+
+    /**
+     *
+     * @return Location, list of 2 doubles. Longitude , Latitude.
+     */
+    public double[] getLocation();
     
 }
--- ParknTravel/src/Main/Update.java
+++ ParknTravel/src/Main/Update.java
@@ -2,6 +2,9 @@
 package Main;
 
 public interface Update {
+    /**
+    * updates the instance
+    **/
     public void update(); // updates 
     
 }
