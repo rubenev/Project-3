c060aea1598182a5cd7345e34b6cbb7727c21bf4
danny <dannydegraaf16@hotmail.com>
2016-04-19 11:58:15 +0200

    images

---
+++ ParknTravel/src/Main/Classes/Images.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main.Classes;
+
+import javafx.scene.image.Image;
+
+/**
+ *
+ * @author danny
+ */
+public class Images {
+    public static Image logo = new Image("Images/Halfvol.png");
+    public static Image background = new Image("Images/mapRotterdam.png");
+    public static Image up_image = new Image("Images/up_button.png");
+    public static Image down_image = new Image("Images/down_button.png");
+    public static Image left_image = new Image("Images/left_button.png");
+    public static Image right_image = new Image("Images/right_button.png");
+    public static Image menu_image = new Image("Images/menuimg.png");
+    public static Image looptijd_image = new Image("Images/looptijd.png");
+    public static Image loopafstand_image = new Image("Images/loopafstand.png");
+    public static Image bus_imagebw = new Image("Images/B-location_imagebw.png");
+    public static Image tram_imagebw = new Image("Images/T-location_imagebw.png");
+    public static Image metro_imagebw = new Image("Images/M-location_imagebw.png");
+    public static Image B_image_info = new Image("Images/B-location_image_info.png"); //TEWST
+    
+}
