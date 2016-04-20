dbed477664b2df63dd198c97f8a02f6774b9d880
rubenev <mac_fley@hotmail.com>
2016-04-20 21:04:25 +0200

    exitbutton update

--- ParknTravel/src/Main/exitButton.java
+++ ParknTravel/src/Main/exitButton.java
@@ -21,38 +21,44 @@ public class exitButton {
          double canvas_y;
          double canvas_x;
          boolean checked = true;
+         
          public exitButton(Group root, double canvas_y, double canvas_x,Stage primaryStage, Button up, Button down, Button left, Button right ){
-         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();    
-         exitButton.setLayoutX(canvas_y - 80);
-         exitButton.setLayoutY(10);
-         exitButton.setGraphic(new ImageView("Images/exit_fullscreen.png"));
-         exitButton.setStyle("-fx-background-color: transparent");
-         exitButton.setOnAction(e -> { 
-            if(checked){
-            exitButton.setGraphic(new ImageView("Images/enter_fullscreen.png"));
-            primaryStage.setFullScreen(false);
-            primaryStage.setWidth(1500);
-            primaryStage.setHeight(1000);
-            this.canvas_y = 1500;
-            this.canvas_x = 1000;
-            down.setLayoutY((this.canvas_x-115));
-            right.setLayoutX(this.canvas_y-102);
-            exitButton.setLayoutX(this.canvas_y - 100);
-            checked=false;}else{
-            exitButton.setGraphic(new ImageView("Images/exit_fullscreen.png"));
-            primaryStage.setFullScreen(true);
-            this.canvas_y = screenSize.getWidth();
-            this.canvas_x = screenSize.getHeight();
-            down.setLayoutY((canvas_x-75));
-            right.setLayoutX(canvas_y-82);
-            exitButton.setLayoutX(canvas_y - 80);
-            checked=true;}
+            Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();    
+            this.exitButton.setLayoutX(canvas_y - 80);
+            this.exitButton.setLayoutY(10);
+            this.exitButton.setGraphic(new ImageView("Images/exit_fullscreen.png"));
+            this.exitButton.setStyle("-fx-background-color: transparent");
+            this.exitButton.setOnAction(e -> { 
+               if(this.checked)
+               {
+               this.exitButton.setGraphic(new ImageView("Images/enter_fullscreen.png"));
+               primaryStage.setFullScreen(false);
+               primaryStage.setWidth(1500);
+               primaryStage.setHeight(1000);
+               this.canvas_y = 1500;
+               this.canvas_x = 1000;
+               down.setLayoutY((this.canvas_x-115));
+               right.setLayoutX(this.canvas_y-102);
+               this.exitButton.setLayoutX(this.canvas_y - 100);
+               this.checked=false;
+               }
+               else
+               {
+               this.exitButton.setGraphic(new ImageView("Images/exit_fullscreen.png"));
+               primaryStage.setFullScreen(true);
+               this.canvas_y = screenSize.getWidth();
+               this.canvas_x = screenSize.getHeight();
+               down.setLayoutY((this.canvas_x-75));
+               right.setLayoutX(this.canvas_y-82);
+               this.exitButton.setLayoutX(this.canvas_y - 80);
+               this.checked=true;
+               }
 
-        right.setLayoutY((canvas_x/2)-20);
-        down.setLayoutX((canvas_y/2)-20);           
-        left.setLayoutY((canvas_x/2)-20);
-        up.setLayoutX((canvas_y/2)-20);       
-         });
+            right.setLayoutY((this.canvas_x/2)-20);
+            down.setLayoutX((this.canvas_y/2)-20);           
+            left.setLayoutY((this.canvas_x/2)-20);
+            up.setLayoutX((this.canvas_y/2)-20);       
+             });
 
 }
         public Button getExitbutton(){
